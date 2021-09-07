5390e0ba9297fa4ac7c37f649f47422974ddc228
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-07 18:09:24 +0530

    WelCome Message Added

---
+++ RefactorEmpWage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RefactorEmpWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wel-Come to Employee Wage Computation Program");
+        }
+    }
+}
